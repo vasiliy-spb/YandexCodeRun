32279ba3c1c87acf8befc7e3b0c9c11e3c633d2f
vasiliy-spb <vasiliy-spb07@mail.ru>
2023-06-03 23:04:19 +0700

    Trying to solve 1

--- src/Программист_на_пляже/CoderOnTheBeach.java
+++ src/Программист_на_пляже/CoderOnTheBeach.java
@@ -1,12 +1,45 @@
 package Программист_на_пляже;
 
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class CoderOnTheBeach {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        int a = 0b00000010;
-        int b = 0b00000100;
+        String inputFile = "/Users/vasiliy/IdeaProjects/YandexCodeRun/src/Программист_на_пляже/input.txt";
+        String outputFile = "/Users/vasiliy/IdeaProjects/YandexCodeRun/src/Программист_на_пляже/output.txt";
 
-        int result = a ^ b;
-        System.out.println(Integer.toBinaryString(result));
+        BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+
+        PrintWriter writer = new PrintWriter(outputFile);
+
+        int testCount = Integer.parseInt(reader.readLine());
+        if (testCount < 1 || testCount > 1000) return;
+
+        for (int i = 0; i < testCount; i++) {
+            int countSunbed = Integer.parseInt(reader.readLine());
+            if (countSunbed < 2 || countSunbed > 1_000_000) return;
+            String[] sunbeds = reader.readLine().split(" ");
+
+            int a = Integer.parseInt(sunbeds[0]);
+            if (a < 0 || a > 1_000_000_000) return;
+            int b = Integer.parseInt(sunbeds[1]);
+            if (b < 0 || b > 1_000_000_000) return;
+            int min = a ^ b;
+            for (int j = 2; j < countSunbed; j++) {
+                a = Integer.parseInt(sunbeds[j - 1]);
+                b = Integer.parseInt(sunbeds[j]);
+                if (b < 0 || b > 1_000_000_000) return;
+                int currentDiff = a ^ b;
+                if (currentDiff < min) min = currentDiff;
+            }
+            writer.println(min);
+        }
+        reader.close();
+        writer.close();
+
     }
 }
